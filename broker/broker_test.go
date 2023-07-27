932b0ebdf3cd32d7b29d7e6635da7407ed07b8c2
east-true <leik24@naver.com>
2023-07-27 12:32:50 +0900

    add broker test code

--- broker/broker_test.go
+++ broker/broker_test.go
@@ -2,9 +2,22 @@ package broker_test
 
 import (
 	. "mqttgo/broker"
+	"mqttgo/broker/opt"
+	"mqttgo/topic"
 	"testing"
 )
 
 func TestBroker(t *testing.T) {
+	topic := &topic.Topic{
+		Topic: "/machbase/test/#",
+		Qos:   1,
+	}
+	opt := new(opt.Option)
 
+	broker := New("127.0.0.1:503", topic)
+	if err := broker.Connect(opt.None()); err != nil {
+		t.Error(err)
+	}
+
+	broker.Disconnect()
 }
